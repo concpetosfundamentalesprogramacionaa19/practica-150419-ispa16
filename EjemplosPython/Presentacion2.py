ea9e7577f6d0771a41ab5b8f80f54ffecd09fbbe
ispa16 <ispa16@hotmail.com>
2019-04-15 16:49:04 -0500

    ejemplos de clase

---
+++ EjemplosPython/Presentacion2.py
@@ -0,0 +1,9 @@
+"""
+este es el tercer ejemplo
+ingreso de informacion por teclado
+"""
+nombre = input("por favor ingrese su nombre :\n")
+apellido = input("por favor ingrese su apellido :\n")
+
+# aca abajo esta el codigo para imprimmir :V
+print("Mi nombre es : %s \n \t Mi apellido es : \n%s" %(nombre,apellido))
